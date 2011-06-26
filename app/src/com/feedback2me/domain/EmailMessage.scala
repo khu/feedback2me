967f2eeedc9ed0055966b6d7d2586681053b1be7
TWer <twer@106-11-4.local>
2011-06-26 18:18:09 +0800

    be able to parse the Email and Emails to Json object.

--- app/src/com/feedback2me/domain/EmailMessage.scala
+++ app/src/com/feedback2me/domain/EmailMessage.scala
@@ -3,12 +3,11 @@ package com.feedback2me.domain
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
-class EmailMessage(val from:String,
-                   val to:String,
-                   val subject:String,
-                   val content:String,
-                   val receivedDate:DateTime) {
-  //1995-12-04 12:00:00
+class EmailMessage(val from: String,
+                   val to: String,
+                   val subject: String,
+                   val content: String,
+                   val receivedDate: DateTime) {
   def toJson = {
     String.format("""{
       "id": "%s",
@@ -20,7 +19,7 @@ class EmailMessage(val from:String,
       "importance": 50,
       "icon":"triangle_orange.png"
     }""", receivedDate.getMillis.toString, subject, content,
-        receivedDate.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")),
-        receivedDate.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")))
+      receivedDate.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")),
+      receivedDate.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")))
   }
 }
--- app/src/com/feedback2me/domain/EmailMessages.scala
+++ app/src/com/feedback2me/domain/EmailMessages.scala
@@ -1,7 +1,18 @@
 package com.feedback2me.domain
 
-class EmailMessages {
-  def toJson = {
 
+class EmailMessages {
+  var messages: List[EmailMessage] = List[EmailMessage]()
+
+  def add(mail: EmailMessage) {
+    messages = messages ::: List(mail)
+  }
+
+  def toJson = {
+    var list = List[String]()
+    for (message <- messages) {
+      list = list ::: List(message.toJson)
+    }
+    list.mkString("[", ",", "]")
   }
 }
