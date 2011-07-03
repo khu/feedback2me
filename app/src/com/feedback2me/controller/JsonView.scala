f79e4a2230548943918d28acb1251fbfde649da0
TWer <twer@106-11-4.local>
2011-07-03 12:25:18 +0800

    fixed the encoding problem.

--- app/src/com/feedback2me/controller/JsonView.scala
+++ app/src/com/feedback2me/controller/JsonView.scala
@@ -10,12 +10,15 @@ class JsonView(val json:String) extends View {
   
   @Override
   def render(model: Map[String, _], reqest: HttpServletRequest, response: HttpServletResponse) {
+    //setContentType("text/html; charset=UTF-8"); response.setCharacterEncoding("UTF-8 ");
+    response.setCharacterEncoding("UTF-8")
     val printWriter: PrintWriter = response.getWriter
     printWriter.write(this.json);
     printWriter.flush
     printWriter.close
   }
+
   @Override
-  def getContentType = {"text/x-json"}
+  def getContentType = {"text/x-json; charset=UTF-8"}
 
 }
